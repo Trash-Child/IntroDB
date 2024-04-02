49209599a3fa330863e373db1119798dfb41ed5f
ChristofferBuhl <124875038+ChristofferBuhl@users.noreply.github.com>
2024-04-02 14:14:32 +0200

    Updated trigger

--- TriggerNewspaperDB.sql
+++ TriggerNewspaperDB.sql
@@ -2,7 +2,7 @@ DELIMITER //
 CREATE TRIGGER ArticleBeforeEdition
 BEFORE INSERT ON Article FOR EACH ROW
 BEGIN
-IF NEW.ArticleDate < OLD.Edition.PubDate THEN SET NEW.ArticleDate = '0000-00-00 00:00:00';
+IF NEW.ArticleDate < Edition.PubDate THEN SET NEW.ArticleDate = '0000-00-00 00:00:00';
 END IF;
 END//
 DELIMITER ;
